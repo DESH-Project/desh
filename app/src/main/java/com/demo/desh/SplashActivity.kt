96f62de4ed41fd64c6dd73e87a4786fc739beb33
seungsu <h970126@gmail.com>
2023-07-20 23:44:17 +0900

    #1 - Splash API 적용

--- app/src/main/java/com/demo/desh/SplashActivity.kt
+++ app/src/main/java/com/demo/desh/SplashActivity.kt
@@ -1,13 +1,20 @@
 package com.demo.desh
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
@@ -18,7 +25,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         lifecycleScope.launchWhenCreated {
-            delay(3000)
+            delay(2500)
 
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
