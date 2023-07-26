443a6d30f52c20f09a84414a8014e6b7f350d192
seungsu <h970126@gmail.com>
2023-07-26 10:10:23 +0900

    #1 - Coil Image API

--- app/src/main/java/com/demo/desh/MainActivity.kt
+++ app/src/main/java/com/demo/desh/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
+import com.demo.desh.dto.User
 import com.demo.desh.ui.screen.MainActivityScreen
 import com.demo.desh.ui.theme.DeshprojectfeTheme
 
@@ -11,10 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val user = intent.getSerializableExtra("user") as User
+
         setContent {
             DeshprojectfeTheme {
                 Surface {
-                    MainActivityScreen()
+                    MainActivityScreen(this, user)
                 }
             }
         }
--- app/src/main/java/com/demo/desh/ui/screen/MainScreen.kt
+++ app/src/main/java/com/demo/desh/ui/screen/MainScreen.kt
@@ -1,19 +1,34 @@
 package com.demo.desh.ui.screen
 
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.rememberAsyncImagePainter
+import com.demo.desh.dto.User
 import com.demo.desh.ui.theme.DeshprojectfeTheme
 
 @Composable
-fun MainActivityScreen() {
+fun MainActivityScreen(context: Context, user: User) {
     Text(text = "MainScreen")
+
+    Column {
+        Text(text = user.id!!.toString())
+        Text(text = user.email)
+        Text(text = user.nickname)
+        
+        Image(
+            painter = rememberAsyncImagePainter(model = user.profileImageUrl),
+            contentDescription = ""
+        )
+    }
 }
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 private fun MainActivityScreenPreview() {
     DeshprojectfeTheme {
-        MainActivityScreen()
     }
 }
