0c286989725e601d3c8a0d54e994672dc8734f5b
seungsu <h970126@gmail.com>
2023-08-01 10:51:27 +0900

    #1 - Navigation

--- app/src/main/java/com/demo/desh/MainActivity.kt
+++ app/src/main/java/com/demo/desh/MainActivity.kt
@@ -10,8 +10,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
 import com.demo.desh.model.User
+import com.demo.desh.screens.Home
+import com.demo.desh.screens.Profile
+import com.demo.desh.screens.Welcome
 import com.demo.desh.ui.theme.DeshprojectfeTheme
 
 class MainActivity : AppCompatActivity() {
@@ -23,15 +29,22 @@ class MainActivity : AppCompatActivity() {
         setContent {
             DeshprojectfeTheme {
                 Surface {
-                    MainActivityScreen(this, user)
+                    MainActivityScreen(user)
                 }
             }
         }
     }
 }
 
+sealed class NavRoutes(val route: String) {
+    object Home : NavRoutes("home")
+    object Welcome : NavRoutes("welcome")
+    object Profile : NavRoutes("profile")
+}
+
 @Composable
-fun MainActivityScreen(context: Context, user: User) {
+fun MainActivityScreen(user: User) {
+    /*
     Text(text = "MainScreen")
 
     Column {
@@ -43,6 +56,27 @@ fun MainActivityScreen(context: Context, user: User) {
             painter = rememberAsyncImagePainter(model = user.profileImageUrl),
             contentDescription = ""
         )
+    }
+     */
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.Home.route
+    ) {
+        composable(route = NavRoutes.Home.route) {
+            Home(navController = navController)
+        }
+
+        composable(route = NavRoutes.Welcome.route + "/{userName}") { backStackEntry ->
+            val userName = backStackEntry.arguments?.getString("userName")
+            Welcome(navController = navController, userName = userName)
+        }
+
+        composable(route = NavRoutes.Profile.route) {
+            Profile()
+        }
     }
 }
 
