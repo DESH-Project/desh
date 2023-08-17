365eecbdd4242c062dfee642b7d6c1ba62629bfa
seungsu <h970126@gmail.com>
2023-08-17 17:26:15 +0900

    #1 - Map Service List 연동

--- app/src/main/java/com/demo/desh/ui/screens/MapScreen.kt
+++ app/src/main/java/com/demo/desh/ui/screens/MapScreen.kt
@@ -2,18 +2,23 @@ package com.demo.desh.ui.screens
 
 import android.content.Context
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import net.daum.mf.map.api.MapView
 
 
 @Composable
 fun MapScreen(mapView: (context: Context) -> MapView, serviceList: List<String>) {
+    val size = serviceList.size
+
     Column {
         CreateListButton(serviceList)
 
@@ -24,9 +29,20 @@ fun MapScreen(mapView: (context: Context) -> MapView, serviceList: List<String>)
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun CreateListButton(serviceList: List<String>) {
-    Row {
-        serviceList.forEach { item -> TextButton(onClick = { }, content = { Text(text = item) }) }
+    LazyRow {
+        itemsIndexed(serviceList) { idx, item ->
+            Card(
+                onClick = { },
+
+                modifier = Modifier,
+
+                elevation = 6.dp
+            ) {
+                Text(text = item)
+            }
+        }
     }
 }
